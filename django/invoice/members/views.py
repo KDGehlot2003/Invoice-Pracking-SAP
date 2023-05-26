6af79b62a6a6ae2f24cb4aacf8acc1e50cf56efb
Kshitij Gehlot <kdgehlot2003@gmail.com>
2023-05-26 23:36:31 +0530

    modified

--- django/invoice/members/views.py
+++ django/invoice/members/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from .models import Member
 
 def members(request):
-    template = loader.get_template('myfirst.html')
-    return HttpResponse(template.render())
+    mymembers = Member.objects.all().values()
+    template = loader.get_template('all_members.html')
+    context ={
+        'mymembers': mymembers
+    }
+    return HttpResponse(template.render(context, request))
