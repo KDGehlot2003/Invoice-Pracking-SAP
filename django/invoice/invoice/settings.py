0594dc3fee6ee92b14f80e26ccc18247f88ce5df
Kshitij Gehlot <kdgehlot2003@gmail.com>
2023-05-28 22:29:13 +0530

    till admin from w3 schools

--- django/invoice/invoice/settings.py
+++ django/invoice/invoice/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-vg=!(m71i0wfw11e=w=i8ylfv$yzpjzvc44w!e=g%owv-=9$$3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
